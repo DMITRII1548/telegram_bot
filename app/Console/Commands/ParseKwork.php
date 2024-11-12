c00e2e6f6b6f00212c924bee7102adbf8d1504d7
DMITRII1548 <korsunov2006@list.ru>
2024-11-12 22:12:57 +0600

    Create parser

--- app/Console/Commands/ParseKwork.php
+++ app/Console/Commands/ParseKwork.php
@@ -3,12 +3,17 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Concurrency;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Symfony\Component\CssSelector\Node\FunctionNode;
 use Symfony\Component\DomCrawler\Crawler;
 
 class ParseKwork extends Command
 {
+    private int $lastPage = 1;
+    private array $projects;
+
     /**
      * The name and signature of the console command.
      *
@@ -28,7 +33,48 @@ class ParseKwork extends Command
      */
     public function handle()
     {
-        $response = Http::get('https://kwork.ru/projects');
+        $projects = [];
+
+        $lastPage = $this->getLastPage();
+
+        $actions = [];
+
+        for ($i = 1; $i <= $lastPage; $i++) {
+            $actions[] = function () use ($i) {
+                $response = Http::get("https://kwork.ru/projects?price-to=15000&keyword=laravel+лара+ларавел+ларавель+php+figma&page=$i");
+                $html = (string)$response->body();
+
+                $crawler = new Crawler($html);
+
+                $script = $crawler->filter('script')->eq(11)->text();
+
+                if (preg_match('/window\.stateData\s*=\s*(\{.*\});/s', $script, $matches)) {
+                    $script = $matches[1];
+
+                    $json = json_decode($script, true);
+
+                    foreach ($json['wants'] as $project) {
+                        $projects[] = [
+                            'id' => $project['id'],
+                            'title' => $project['name'],
+                            'description' => $project['description'],
+                            'price' => $project['priceLimit'],
+                            'username' => $project['user']['username'],
+                        ];
+                    }
+                }
+
+                return $projects;
+            };
+        }
+
+
+        $projects = Concurrency::run($actions);
+    }
+
+    private function getLastPage(): int
+    {
+        $response = Http::get("https://kwork.ru/projects?price-to=15000&keyword=laravel+лара+ларавел+ларавель+php+figma");
         $html = (string)$response->body();
 
         $crawler = new Crawler($html);
@@ -39,6 +85,8 @@ class ParseKwork extends Command
             $script = $matches[1];
 
             $json = json_decode($script, true);
+
+            return $json['pagination']['last_page'];
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,28 @@
 <?php
 
 use App\Facades\Telegram;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\DomCrawler\Crawler;
+
+
 
 Route::get('/', function () {
+    $response = Http::get('https://kwork.ru/projects');
+$html = (string)$response->body();
+
+$crawler = new Crawler($html);
+
+$script = $crawler->filter('script')->eq(11)->text();
+
+if (preg_match('/window\.stateData\s*=\s*(\{.*\});/s', $script, $matches)) {
+    // $matches[1] теперь содержит сам JSON-объект в виде строки
+    $script = $matches[1];
+
+    // Декодируем JSON в PHP массив
+    $json = json_decode($script, true);
+
+    dd($json);
+}
     return view('welcome');
 });
